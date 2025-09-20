c6a6e9afeaba78d94323a24083bf59666be023af
User <user@MacBook-Pro-de-User.local>
2025-09-20 21:59:53 +0100

    create adminInitilizar via sistema

---
+++ src/main/java/pt/com/javadevweek/smartdelivery/cadastro/infra/security/AdminiInitilizarConfig.java
@@ -0,0 +1,21 @@
+package pt.com.javadevweek.smartdelivery.cadastro.infra.security;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import pt.com.javadevweek.smartdelivery.cadastro.model.userEntity.Roles;
+import pt.com.javadevweek.smartdelivery.cadastro.useCase.useCaseUser.UserCaseEntity;
+
+@Configuration
+public class AdminiInitilizarConfig {
+    
+  //  @Bean
+    public CommandLineRunner initAdmin(UserCaseEntity userCaseEntity){
+        return args -> {
+            userCaseEntity.execute("admin@smartdelivery.com", "admin123", Roles.ADMIN);
+        };
+
+    }
+    
+}
