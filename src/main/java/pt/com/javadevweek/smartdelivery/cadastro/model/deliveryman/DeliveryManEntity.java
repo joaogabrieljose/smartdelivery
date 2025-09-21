be7ca7c75bb96dca5d2b239f44ce817d434d1182
User <user@MacBook-Pro-de-User.local>
2025-09-21 19:02:08 +0100

    DeliveryManEntity

---
+++ src/main/java/pt/com/javadevweek/smartdelivery/cadastro/model/deliveryman/DeliveryManEntity.java
@@ -0,0 +1,27 @@
+package pt.com.javadevweek.smartdelivery.cadastro.model.deliveryman;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_delivery_man")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryManEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private String document;
+    private String phone;
+    
+}
