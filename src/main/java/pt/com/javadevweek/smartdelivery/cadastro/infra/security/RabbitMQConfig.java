f82558d7b8655ea7fbb0465aee45d417f861da37
User <user@MacBook-Pro-de-User.local>
2025-09-21 21:09:49 +0100

    MessageConvert para serialização/deserialização

--- src/main/java/pt/com/javadevweek/smartdelivery/cadastro/infra/security/RabbitMQConfig.java
+++ src/main/java/pt/com/javadevweek/smartdelivery/cadastro/infra/security/RabbitMQConfig.java
@@ -1,12 +1,10 @@
 package pt.com.javadevweek.smartdelivery.cadastro.infra.security;
 
-
-
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter; 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-
 
 @Configuration
 public class RabbitMQConfig {
@@ -14,8 +12,12 @@ public class RabbitMQConfig {
     public static final String QUEUE_ORDER_CREATED = "pedido_criado";
 
     @Bean
-    public Queue queue(){
+    public Queue queue() {
         return new Queue(QUEUE_ORDER_CREATED, true);
     }
-    
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 }
