1aa3bc52f2cacd761b057b708bdd2391e1321a8b
User <user@MacBook-Pro-de-User.local>
2025-09-21 19:28:14 +0100

    update UseCaseDelivery usando buil()

--- src/main/java/pt/com/javadevweek/smartdelivery/cadastro/useCase/useCaseDelivery/UseCaseDeliveryMan.java
+++ src/main/java/pt/com/javadevweek/smartdelivery/cadastro/useCase/useCaseDelivery/UseCaseDeliveryMan.java
@@ -3,16 +3,32 @@ package pt.com.javadevweek.smartdelivery.cadastro.useCase.useCaseDelivery;
 import org.springframework.stereotype.Service;
 
 import pt.com.javadevweek.smartdelivery.cadastro.model.deliveryman.DeliverManRepository;
+import pt.com.javadevweek.smartdelivery.cadastro.model.deliveryman.DeliveryManEntity;
 import pt.com.javadevweek.smartdelivery.cadastro.model.deliveryman.dto.CreateDeliveryManRequest;
 
 @Service
 public class UseCaseDeliveryMan {
 
+    private DeliverManRepository deliverManRepository;
 
+    public UseCaseDeliveryMan(DeliverManRepository deliverManRepository){
+        this.deliverManRepository = deliverManRepository;
+    }
 
 
     public void execute(CreateDeliveryManRequest deliveryManRequest){
 
+        this.deliverManRepository.findByDocument(deliveryManRequest.document()).ifPresent(use -> {
+            throw new IllegalArgumentException("entregador já cadastrado...");
+        });
+
+        DeliveryManEntity entity = new DeliveryManEntity().builder()
+            .name(deliveryManRequest.name())
+            .document(deliveryManRequest.document())
+            .phone(deliveryManRequest.phone())
+            .build();
+            this.deliverManRepository.save(entity);
     }
+
     
 }
